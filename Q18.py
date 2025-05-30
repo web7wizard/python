b093052320a9e99e9ef779cb3f969f2eb164e75c
Jiya <164993852+web7wizard@users.noreply.github.com>
2025-05-30 23:08:50 +0530

    Add files via upload

---
+++ Q18.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\n")
+
+j=1
+for i in range(1,10):
+    while(j):
+        print(i,end="")
+
+    print("\n")
